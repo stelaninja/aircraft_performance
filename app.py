9b352a9d27dd31ca1f9bd86ff9ad0f36fa3aaf4e
stelaninja <stefan@johansson.com>
2021-02-22 17:10:16 +0100

    Added code for Heroku databas

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from website import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=True)
+    app.run(debug=True)
--- website/__init__.py
+++ website/__init__.py
@@ -6,14 +6,17 @@ from flask_login import LoginManager
 # from .secret_key import SECRET_KEY
 import os
 
-db = SQLAlchemy()
+db = SQLAlchemy(app)
 DB_NAME = "aircraft_database.db"
 
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", None)
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    # app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    app.config[
+        "SQLALCHEMY_DATABASE_URI"
+    ] = f"postgres://cqnjuhzztzxozx:710bd51c7319105c98c2f9d8f7430b2c1ab9400bad0c33f12978133d6da4d317@ec2-63-34-97-163.eu-west-1.compute.amazonaws.com:5432/d28ogknce5g596"
     app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
