5cb5ac210ccab14ca7c1a9a3b88da416d228de43
stelaninja <stefan@johansson.com>
2021-02-26 19:48:43 +0100

    Modal for adding envelope and buttons for clearing fields

--- website/static/index.js
+++ website/static/index.js
@@ -33,34 +33,11 @@ function hideSpinner() {
 var loadpointModal = $("#loadpointModal");
   $(function () {
 
-      //JSON data by API call
-      // $.get(productListApiUrl, function (response) {
-      //     if(response) {
-      //         var table = '';
-      //         $.each(response, function(index, product) {
-      //             table += '<tr data-id="'+ product.product_id +'" data-name="'+ product.name +'" data-unit="'+ product.uom_id +'" data-price="'+ product.price_per_unit +'">' +
-      //                 '<td>'+ product.name +'</td>'+
-      //                 '<td>'+ product.uom_name +'</td>'+
-      //                 '<td>'+ product.price_per_unit +'</td>'+
-      //                 '<td><span class="btn btn-xs btn-danger delete-product">Delete</span></td></tr>';
-      //         });
-      //         $("table").find('tbody').empty().html(table);
-      //     }
-
-
-      // var table = "";
-      // table += '<tr data-id="1" data-name="Namn" data-unit="Enhet" data-price="100">' +
-      //     '<td>Namn</td>' +
-      //     '<td>Enhet</td>' +
-      //     '<td>100</td>' +
-      //     '<td><span class="btn btn-xs btn-danger delete-product">Delete</span></td></tr>';
-      //     $("table").find('tbody').empty().html(table);
-      // });
   });
 
   // Save Load Point
   $("#saveLoadpoint").on("click", function () {
-    // If we found id value in form then update product detail
+    
     // var data = $("#loadpointForm").serializeArray();
     var data = $("#loadpointForm").serializeArray();
 
@@ -97,6 +74,75 @@ var loadpointModal = $("#loadpointModal");
 });
 
   loadpointModal.on('show.bs.modal', function(){
+    //JSON data by API call
+    // $.get(uomListApiUrl, function (response) {
+    //     if(response) {
+    //         var options = '<option value="">--Select--</option>';
+    //         $.each(response, function(index, uom) {
+    //             options += '<option value="'+ uom.uom_id +'">'+ uom.uom_name +'</option>';
+    //         });
+    //         $("#uoms").empty().html(options);
+    //     }
+    var options = '<option value="">--Select--</option>';
+    
+    options += '<option value="enhet">Namn</option>';
+    $("#uoms").empty().html(options);
+    // });
+  });
+
+function clearEnvelope() {
+  var envelope_field = document.getElementById("envelope");
+  envelope_field.value = "";
+};
+
+function clearLoadpoints() {
+  var envelope_field = document.getElementById("loading_points");
+  envelope_field.value = "";
+};
+
+var envelopeModal = $("#envelopeModal");
+$(function () {
+
+
+  });
+
+  // Save Load Point
+  $("#saveEnvelope").on("click", function () {
+    var data = $("#envelopeForm").serializeArray();
+
+    for (var i=0;i<data.length;++i) {
+        var element = data[i];
+        switch(element.name) {
+            case 'weight':
+                var weight = element.value;
+                break;
+            case 'cg_envelope':
+                var cg = element.value;
+                break;
+        }
+    }
+    var envelope_field = document.getElementById("envelope");
+    try {
+      var envelope = envelope_field.value;
+    } catch(e) {
+      // alert(e)
+      var envelope = ""
+    }
+    if (envelope.length == 0) {
+      var envelope = ""
+    };
+    console.log(envelope)
+    envelope += "(" + parseFloat(cg) + "," + weight + "),";
+
+    envelope_field.value = envelope;
+    console.table(envelope)
+
+    // callApi("POST", productSaveApiUrl, {
+    //     'data': JSON.stringify(requestPayload)
+    // });
+});
+
+  envelopeModal.on('show.bs.modal', function(){
     //JSON data by API call
     // $.get(uomListApiUrl, function (response) {
     //     if(response) {
